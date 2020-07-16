796fda7cb07b7aa092e5756165086858c7bb9e5e
xubowen0601 <786875770@qq.com>
2020-07-16 17:19:18 +0800

    增加运算

--- 窗口HelloWorld/窗口HelloWorld/Demo.cpp
+++ 窗口HelloWorld/窗口HelloWorld/Demo.cpp
@@ -1,9 +1,9 @@
-#include<Windows.h>				//一个Windows应用程序应该包含的头文件
-#include<stdio.h>				//标准输入输出流
-LRESULT CALLBACK WinSunProc		/*声明一个回调函数*/
+#include<Windows.h>						//一个Windows应用程序应该包含的头文件
+#include<stdio.h>						//标准输入输出流
+LRESULT CALLBACK WinSunProc				/*声明一个回调函数*/
 (
-	HWND hwnd,      			/*窗口的句柄*/
-	UINT uMsg,      			/*窗口的消息*/
+	HWND hwnd,      					/*窗口的句柄*/
+	UINT uMsg,      					/*窗口的消息*/
 	WPARAM wParam,
 	LPARAM lParam
 );
@@ -18,9 +18,9 @@ int WINAPI WinMain
 	LPSTR lpCmdLine,          			//储存一个命令行参数
 	int nCmdShow)
 {
-	WNDCLASS wndcls;				//定义一个窗口对象
-	wndcls.cbClsExtra = 0;			//指定额外内存空间
-	wndcls.cbWndExtra = 0;			//指定额外内存空间
+	WNDCLASS wndcls;					//定义一个窗口对象
+	wndcls.cbClsExtra = 0;				//指定额外内存空间
+	wndcls.cbWndExtra = 0;				//指定额外内存空间
 	//指定窗口背景色
 	wndcls.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	//设置光标样式
@@ -31,11 +31,11 @@ int WINAPI WinMain
 	wndcls.lpfnWndProc = WinSunProc;	//指定窗口函数，即窗口主处理函数
 	//窗口类名称
 	wndcls.lpszClassName = "Visual C++ Game";
-	wndcls.lpszMenuName = NULL;		//菜单
+	wndcls.lpszMenuName = NULL;			//菜单
 	wndcls.style = CS_HREDRAW | CS_VREDRAW;
 	RegisterClass(&wndcls); 			//注册窗口类 
-	HWND hwnd;					//声明窗口句柄
-	hwnd = CreateWindow(			/*创建窗口，但这里的窗口是不会显示的*/
+	HWND hwnd;							//声明窗口句柄
+	hwnd = CreateWindow(				/*创建窗口，但这里的窗口是不会显示的*/
 		"Visual C++ Game", 				/*已注册窗口类的名称*/
 		"Visual C++ 游戏开发",			/*窗口标题*/
 		WS_OVERLAPPEDWINDOW,			/*窗口风格*/
@@ -49,14 +49,14 @@ int WINAPI WinMain
 		NULL);
 	//在这里真正显示窗口
 	ShowWindow(hwnd, SW_SHOWNORMAL);
-	UpdateWindow(hwnd);
+	UpdateWindow(hwnd);					//更新显示
 	/*
 		初始化工作完成后，WinMain进入所谓的消息循环
 	*/
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		TranslateMessage(&msg);			//转换键盘消息
+		DispatchMessage(&msg);			//分派消息
 	}
 	return 0;
 }
@@ -71,19 +71,28 @@ LRESULT CALLBACK WinSunProc(
 	LPARAM lParam						/*参数2*/
 )
 {
+	char tmsg[128] = { 0 };
+	int num1, num2, num3, num4, num5;	//声明5个变量
+	num1 = 3 + 8;						//加法运算
+	num2 = 10 - 7;						//减法运算
+	num3 = 100 * 33;					//乘法运算
+	num4 = 155 / 5;						//除法运算
+	num5 = 9 % 2;						//取模运算
+										//把运算符和结果输出到tmsg中
+	sprintf_s(tmsg, "3+8=%d 10-7=%d 100*33=%d 155/5=%d 9%%2=%d", num1, num2, num3, num4, num5);
 	switch (uMsg)						/*判断消息类型*/
 	{
 	case WM_PAINT:						/*更新窗口信息*/
 		HDC hDC;						/*定义DC设备*/
 		PAINTSTRUCT ps;
 		hDC = BeginPaint(hwnd, &ps);	/*得到设备hDC*/
-		TextOut(hDC, 200, 0, "Visual C++ 游戏开发", strlen("Visual C++ 游戏开发"));
+		TextOut(hDC, 200, 0, tmsg, strlen(tmsg));
 		EndPaint(hwnd, &ps);
 		break;
-	case WM_CLOSE:					/*d当单击关闭按钮时，产生关闭消息*/
+	case WM_CLOSE:						/*d当单击关闭按钮时，产生关闭消息*/
 		if (IDYES == MessageBox(hwnd, "是否真的结束？", "游戏开发", MB_YESNO))
 		{
-			DestroyWindow(hwnd);	/*单击“确定”按钮，销毁窗口*/
+			DestroyWindow(hwnd);		/*单击“确定”按钮，销毁窗口*/
 		}
 		break;
 	case WM_DESTROY:
